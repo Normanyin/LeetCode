dd5e5b15f7301632c6587dde3c0f36b3dcbb091b
yingao163 <yingao163@qq.com>
2015-05-04 23:03:39 +0800

    initial LeetCode version

---
+++ src/com/geekle/problem2/ListNode.java
@@ -0,0 +1,42 @@
+package com.geekle.problem2;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	public ListNode(int x) {
+		val = x;
+		next = null;
+	}
+	
+	public void addNode(int a) {
+		ListNode newNode = new ListNode(a);
+		ListNode p = this;
+		while (p.next != null) {
+			p = p.next;
+		}
+		p.next = newNode;
+		newNode.next = null;
+		
+	}
+	
+	public void insertNode(int a) {
+		if (this.next == null) {
+			this.next = new ListNode(a);
+		} else {
+			ListNode newNode = new ListNode(a);
+			newNode.next = this.next;
+			this.next = newNode;
+		}
+	}
+	
+	public void displayList() {
+		System.out.print("ListNode:");
+		ListNode currentNode = this;
+		System.out.print(currentNode.val);
+		while (currentNode.next != null) {
+			currentNode = currentNode.next;
+			System.out.print("->" + currentNode.val);
+		}
+		System.out.println();
+	}
+}
---
+++ src/com/geekle/problem2/TestNode.java
@@ -0,0 +1,11 @@
+package com.geekle.problem2;
+
+public class TestNode {
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(2);
+		l1.addNode(4);
+		l1.addNode(3);
+		l1.displayList();
+		
+	}
+}
